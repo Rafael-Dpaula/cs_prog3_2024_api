3060fb579b1e48750d08eac633474b39ba96a743
Rafael-Dpaula <rafaeldepaula939@gmail.com>
2024-07-30 11:50:14 -0300

    mudanças no server

--- servidor.js
+++ servidor.js
@@ -15,7 +15,7 @@ sw.use(function (req, res, next) {
 const config = {
     host: 'localhost',
     user: 'postgres',
-    database: 'db_cs_prog3_2024',
+    database: 'db_cs_2024',
     password: 'postgres',
     port: 5432
 };
@@ -29,6 +29,35 @@ sw.get('/', (req, res) => {
 sw.get('/teste', (req, res) => {
     res.send('teste');
 })
+
+sw.get('/listendereco', function (req, res, next) {
+
+    postgres.connect(function (err, client, done) {
+
+        if (err) {
+
+            console.log("Nao conseguiu acessar o  BD " + err);
+            res.status(400).send('{' + err + '}');
+        } else {
+
+            var q = 'select e.codigo, e.complemento, e.cep, j.nickname from tb_endereco e, tb_jogador j where e.nicknamejogador=j.nickname order by e.codigo asc';
+
+            client.query(q, function (err, result) {
+                done(); // closing the connection;
+                if (err) {
+                    console.log('retornou 400 no listendereco');
+                    console.log(err);
+
+                    res.status(400).send('{' + err + '}');
+                } else {
+
+                    //console.log('retornou 201 no /listendereco');
+                    res.status(201).send(result.rows);
+                }
+            });
+        }
+    });
+});
 
 
 
